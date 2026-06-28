6c5b2dbc3d4fb62ecfaec7ca66c706c6aeb932b1
dahernandez2001 <dh25093@gmail.com>
2026-06-28 12:10:38 -0500

    como me va a quedar grande esto

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
 
+from database.database import db
+
 from controllers.home_controller import home_bp
 from controllers.productos_controller import productos_bp
 from controllers.carrito_controller import carrito_bp
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tienda.db"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    from models.producto import Producto
+    db.create_all()
 
 app.register_blueprint(home_bp)
 app.register_blueprint(productos_bp)
--- controllers/productos_controller.py
+++ controllers/productos_controller.py
@@ -2,6 +2,35 @@ from flask import Blueprint, render_template
 
 productos_bp = Blueprint("productos", __name__)
 
+productos = [
+
+    {
+        "nombre":"Chocolate Chips",
+        "precio":8000,
+        "descripcion":"Galleta con chispas de chocolate",
+        "imagen":"galleta1.jpg"
+    },
+
+    {
+        "nombre":"Brownie Cookie",
+        "precio":10000,
+        "descripcion":"Sabor intenso a chocolate",
+        "imagen":"galleta2.jpg"
+    },
+
+    {
+        "nombre":"Red Velvet",
+        "precio":12000,
+        "descripcion":"Nuestra favorita",
+        "imagen":"galleta3.jpg"
+    }
+
+]
+
 @productos_bp.route("/catalogo")
 def catalogo():
-    return render_template("productos/catalogo.html")
+
+    return render_template(
+        "productos/catalogo.html",
+        productos=productos
+    )
